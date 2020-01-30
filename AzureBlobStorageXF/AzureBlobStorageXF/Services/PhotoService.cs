831491bf6e7695ec3bad2d57fd3841de90f52c30
Author Name <jorge@jorgemht.dev>
2020-01-30 23:45:58 +0100

    [Added] - Camara service and permission

---
+++ AzureBlobStorageXF/AzureBlobStorageXF/Services/PhotoService.cs
@@ -0,0 +1,94 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System;
+using System.Threading.Tasks;
+
+namespace AzureBlobStorageXF.Services
+{
+    public class PhotoService
+    {
+        private static PhotoService _instance;
+
+        public static PhotoService Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new PhotoService();
+
+                return _instance;
+            }
+        }
+
+        public async Task<MediaFile> PickPhotoAsync()
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                return null;
+            }
+
+            var status = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Camera);
+
+            if (status != PermissionStatus.Granted)
+            {
+                var results = await CrossPermissions.Current.RequestPermissionsAsync(new[] { Permission.Camera });
+                status = results[Permission.Camera];
+            }
+
+            if (status == PermissionStatus.Granted)
+            {
+                var file = await CrossMedia.Current.PickPhotoAsync();
+
+                if (file == null)
+                {
+                    return null;
+                }
+
+                return file;
+            }
+
+            return null;
+        }
+
+        public async Task<MediaFile> TakePhotoAsync()
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                return null;
+            }
+
+            var status = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Camera);
+
+            if (status != PermissionStatus.Granted)
+            {
+                var results = await CrossPermissions.Current.RequestPermissionsAsync(new[] { Permission.Camera });
+                status = results[Permission.Camera];
+            }
+
+            if (status == PermissionStatus.Granted)
+            {
+                var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+                {
+                    CompressionQuality = 92,
+                    PhotoSize = PhotoSize.Medium,
+                    Name = Guid.NewGuid().ToString()
+                });
+
+                if (file == null)
+                {
+                    return null;
+                }
+
+                return file;
+            }
+
+            return null;
+        }
+    }
+}
